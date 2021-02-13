3d9c727930a927f7004ed6179b4bae7ec3cc8761
JoanRibot <jribot@cifpfbmoll.eu>
2021-02-13 15:55:24 +0100

    Clase Pool conductores con sus casos test

--- edu.pong/src/main/java/conductores/PoolConductores.java
+++ edu.pong/src/main/java/conductores/PoolConductores.java
@@ -1,5 +1,32 @@
 package conductores;
 
+import java.util.List;
+import java.util.Optional;
+
 public class PoolConductores {
-    
+    List<Conductor> conductores;
+
+    public PoolConductores(List<Conductor> conductores) {
+        this.conductores = conductores;
+    }
+
+
+    public List<Conductor> getConductores() {
+        return conductores;
+    }
+
+
+    public Optional<Conductor> asignarConductor() {
+        Optional<Conductor> conductor = this.conductores.stream().filter(c -> !c.isOcupado()).findAny();
+        if(conductor.isPresent()){
+            conductor.get().setOcupado(true);
+            return conductor;
+        }
+        else{
+            System.out.println("Todos los conductores están ocupados");
+            Optional <Conductor> noHayConductores = null;
+            return noHayConductores;
+        }
+    }
+
 }
--- edu.pong/src/test/java/cotxox/PoolConductaresTest.java
+++ edu.pong/src/test/java/cotxox/PoolConductaresTest.java
@@ -1,5 +1,59 @@
 package cotxox;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import conductores.Conductor;
+import conductores.PoolConductores;
+
 public class PoolConductaresTest {
-    
+    PoolConductores conductores;
+
+
+    @Before
+    public void objectsTest(){
+        List<Conductor> poolConductores = new ArrayList<Conductor>();
+        String[] nombreConductores = {"Pere", "Raul", "David"};
+
+        for(String nombre:nombreConductores){
+            Conductor conductor = new Conductor(nombre);
+            poolConductores.add(conductor);
+        }
+
+        conductores = new PoolConductores(poolConductores);
+    }
+
+    @Test
+    public void getConductoresTest(){
+        for (Conductor conductor:conductores.getConductores()){
+            System.out.println(conductor.getNombre());
+        }
+    }
+/**
+*  @Test
+*    public void asignarConductorConSitioTest(){
+*        conductores.asignarConductor();
+*        conductores.asignarConductor();
+*        for (Conductor conductor:conductores.getConductores()){
+*            System.out.println(conductor.isOcupado());
+*        }
+*        
+*    }
+**/
+    @Test
+    public void asignarConductorSinSitioTest(){
+        conductores.asignarConductor();
+        conductores.asignarConductor();
+        conductores.asignarConductor();
+        conductores.asignarConductor();
+        for (Conductor conductor:conductores.getConductores()){
+            System.out.println(conductor.isOcupado());
+        }
+        
+    }
 }
